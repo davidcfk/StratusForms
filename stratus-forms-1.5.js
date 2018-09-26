b87d881d8dacec4b675af4f6c307c2339e4dc7d4
David Kit <davidcfk@gmail.com>
2018-09-27 09:38:44 +1000

    Revert "StratusFormsLookup stores Label, not the Value #29"
    This reverts commit b9ab9cc09faaff80ff39efb5d2ee73b0439bd522.
    
    The change was not necessary, use of SFUseLookupID was sufficient.

--- stratus-forms-1.5.js
+++ stratus-forms-1.5.js
@@ -860,7 +860,7 @@ $.fn.StratusFormsTranslate = function (options)
              $(this).find(":selected").each(function(i, selected){
                 if(value != "")value += ";#";
 
-                value += $(selected).val(); // This will save VALUE of selected instead
+                value += $(selected).text();
             });
             var encryptField = $(this).hasClass(gStratusFormsEncryptClass);
             var encodedValue = htmlEncode(value, encryptField);
